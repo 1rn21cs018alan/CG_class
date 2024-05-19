d2e7277a0a672b735d3564f031fb73657b81e1a3
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-05-19 18:20:47 +0530

    Create Lab_p1.py

---
+++ python/Lab_p1.py
@@ -0,0 +1,30 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+def disp1():
+    glClearColor(0,0,0,1)
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1,0,0)
+    glPointSize(5)
+    glBegin(GL_POINTS)
+    glVertex2f(25,25)
+    glVertex2f(75,75)
+    glEnd()
+    glFlush()
+    
+    
+    
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
+    glutInitWindowPosition(10,10)
+    glutInitWindowSize(700,700)
+    glutCreateWindow(b"points")
+    gluOrtho2D(-200,200,-200,200)
+    glutDisplayFunc(disp1)
+    glutMainLoop()
+    
+    
+    
+main()
