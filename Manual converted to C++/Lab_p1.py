4e875b2e80e7d86b8424d22b422b75e5f828aad2
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-05-19 18:23:51 +0530

    Create Lab_p1.py

---
+++ Manual converted to C++/Lab_p1.py
@@ -0,0 +1,26 @@
+#include<glut.h>
+void disp1(){
+    glClearColor(0,0,0,1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1,0,0);
+    glPointSize(5);
+    glBegin(GL_POINTS);
+    glVertex2f(25,25);
+    glVertex2f(75,75);
+    glEnd();
+    glFlush();
+}
+    
+    
+    
+int main(int argc,char **argv){
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowPosition(10,10);
+    glutInitWindowSize(700,700);
+    glutCreateWindow("points");
+    gluOrtho2D(-200,200,-200,200);
+    glutDisplayFunc(disp1);
+    glutMainLoop();
+    return 0;   
+}
