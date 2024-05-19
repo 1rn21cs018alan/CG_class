7c199841baa28f64b0ef1fc8be5420951b986c4a
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-05-19 18:48:08 +0530

    Update and rename p10.py to p10.cpp

---
+++ Manual converted to C++/p10.cpp
@@ -0,0 +1,30 @@
+#include<glut.h>
+#include<math.h>
+void disp1(){
+    glClearColor(0,0,0,1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINES);
+    glColor3f(1,1,1);
+    int i=0;
+    while(i<=360){
+        double ang=3.1415/180*i;
+        glVertex2f(35*cos(ang),35*sin(ang));
+        glVertex2f(20*cos(ang),20*sin(ang));
+        i+=10;
+    }
+    glEnd();
+    glFlush();
+}
+
+    
+int main(int argc,char **argv){
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowPosition(10,10);
+    glutInitWindowSize(700,700);
+    glutCreateWindow("points");
+    gluOrtho2D(-200,200,-200,200);
+    glutDisplayFunc(disp1);
+    glutMainLoop();
+    return 0;   
+}
