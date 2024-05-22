30c825b34e48e4373f021d3389dd48c7fb1b284a
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-05-22 10:44:44 +0530

    Add files via upload

--- python/lab_p2.py
+++ python/lab_p2.py
@@ -4,28 +4,38 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from math import sin,cos
 
-def DDA_line(x1,y1,x2,y2):
+def Bresen(x1,y1,x2,y2):
     glPointSize(20)
     glBegin(GL_POINTS)
     glColor3f(1,1,1)
     dx=x2-x1
     dy=y2-y1
-    steps=abs(dy)
-    if(abs(dx)>steps):
-        steps=abs(dx)
-    x,y=x1,y1
-    for k in range(steps):
-        x+=dx/steps
-        y+=dy/steps
-        glVertex2i(int(x),int(y))
-        print(int(x),int(y))
+    p=2*dy-dx
+    inc=2*dy
+    dec=2*dy-2*dx
+    if(y1<y2):
+        x,y=x1,y1
+        xend=x2
+    else:
+        x,y=x2,y2
+        xend=x1
+    glVertex2i(x,y)
+    while(x<xend):
+        x+=1
+        if p>=0:
+            p+=dec
+            y+=1
+        else:
+            p+=inc
+        print(x,y)
+        glVertex2i(x,y)
     glEnd()
     glFlush()
     
 def disp1():
     glClearColor(0,0,0,1)
     glClear(GL_COLOR_BUFFER_BIT)
-    DDA_line(-9,-1,9,3)
+    Bresen(-9,-1,9,3)
 
 
 def main():
@@ -33,7 +43,7 @@ def main():
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
     glutInitWindowPosition(10,10)
     glutInitWindowSize(400,400)
-    glutCreateWindow(b"DDA")
+    glutCreateWindow(b"Bresen")
     gluOrtho2D(-10,10,-10,10)
     glutDisplayFunc(disp1)
     glutMainLoop()
