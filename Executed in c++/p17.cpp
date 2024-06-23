b35a199eaa601ed7aa2f8f3966b9609f05a08b2a
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-06-23 20:52:41 +0530

    Add files via upload

---
+++ Executed in c++/p17.cpp
@@ -0,0 +1,61 @@
+#include<GL/glut.h>
+int sizef=5;
+void init() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-300, 300, -300, 300);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void disp() {
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1, 1, 1);
+	glBegin(GL_QUADS);
+	glVertex2i(sizef,sizef);
+	glVertex2i(sizef,-sizef);
+	glVertex2i(-sizef,-sizef);
+	glVertex2i(-sizef,sizef);
+	glEnd();
+	glFlush();
+}
+
+void reshape(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (w > h) {
+		gluOrtho2D(-300 * w / (float)h, 300 * w / (float)h, -300, 300);
+	}
+	else {
+		gluOrtho2D(-300, 300, -300 * h / (float)w, 300 * h / (float)w);
+	}
+	glMatrixMode(GL_MODELVIEW);
+}
+void mouse(int b, int s, int x, int y) {
+	if(s==0)
+		switch (b) {
+		case 0:
+			sizef += 5;
+			break;
+		case 1:
+			sizef = 5;
+			break;
+		case 2:
+			sizef -= 5;
+		}
+}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(300, 300);
+
+	glutCreateWindow("Mouse Control");
+	init();
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutDisplayFunc(disp);
+	glutMainLoop();
+	return 0;
+}
