8af64c2e1e40a72358e0d940cd8a00af353896d1
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-07-01 10:31:04 +0530

    Update and rename 3D_operations.cpp to lab_p4.cpp

--- Executed in c++/lab_p4.cpp
+++ Executed in c++/lab_p4.cpp
@@ -70,9 +70,9 @@ void disp() {
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	cube(0, 0, 0, 0, 0, 0, 0.1, 1.0);
-	cube(0.5, 0, 0, 0, 0, 0, 0.2, 0.9);
-	cube(0, 0.5, 0, 0, 0, 0, 0.2, 0.8);
-	cube(0, 0, 0.5, 0, 0, 0, 0.2, 0.7);
+	//cube(0.5, 0, 0, 0, 0, 0, 0.2, 0.9);
+	//cube(0, 0.5, 0, 0, 0, 0, 0.2, 0.8);
+	//cube(0, 0, 0.5, 0, 0, 0, 0.2, 0.7);
 	glutSwapBuffers();
 }
 void keyboard(unsigned char key, int x, int y) {
